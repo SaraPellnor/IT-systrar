66497a582d834993631d99c8ba126a154ddf2dd7
heinanX <larrii@live.se>
2024-10-24 16:21:27 +0200

    header

--- itsystrar/src/app/components/Header/Header.tsx
+++ itsystrar/src/app/components/Header/Header.tsx
@@ -1,29 +1,29 @@
 import Image, { StaticImageData } from "next/image";
-import txtLogo from "../../assets/logos/logo-text.png";
-import iconLogo from "../../assets/logos/logo-icons.png";
-import MobileHeader from "./MobileHeader";
-import DesktopHeader from "./DesktopHeader";
+import txtLogo from "../../assets/logos/logo-full.png";
+import HeaderNav from "./HeaderNav";
+//import DesktopHeader from "./DesktopHeader";
 
 const listItems = [
-  { title: "Tjänster", slug: "tjanster" },
-  { title: "om oss", slug: "om-oss" },
-  { title: "kontakt", slug: "kontakt" },
-  { title: "projekt", slug: "projekt" },
+  { title: "Tjänster", slug: "services" },
+  { title: "om oss", slug: "about" },
+  { title: "kontakt", slug: "contact" },
+  { title: "projekt", slug: "projects" },
 ];
 
 const Header = ({ headerImage }: { headerImage: StaticImageData }) => {
   return (
-    <div className="h-screen md:h-[50vh] w-screen relative">
-      <MobileHeader
-        txtLogo={txtLogo}
-        iconLogo={iconLogo}
-        listItems={listItems}
-      />
-      <DesktopHeader
-        txtLogo={txtLogo}
-        iconLogo={iconLogo}
-        listItems={listItems}
-      />
+    <div id="topPage" className="h-screen md:h-screen w-screen relative">
+      <div className="absolute z-20 bg-gradient-to-b from-zinc-900/50 to-slate-200/0 py-8 w-screen h-screen flex  flex-col justify-center">
+        <section className=" absolute top-0 left-0 flex gap-2 w-full justify-center pt-5">
+          <Image
+            src={txtLogo}
+            alt="ITsystrar"
+            className="h-14 lg:h-20 w-auto"
+          />
+        </section>
+
+        <HeaderNav listItems={listItems} />
+      </div>
 
       <Image
         src={headerImage}
