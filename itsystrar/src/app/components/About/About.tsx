94df6308b14ea7dd5b8a3c334852870ff29efa1d
Sara Pellnor <sara.ahlquist@hotmail.com>
2024-10-26 15:02:29 +0200

    responsiv

--- itsystrar/src/app/components/About/About.tsx
+++ itsystrar/src/app/components/About/About.tsx
@@ -1,34 +1,40 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import React from "react";
-import aboutImage from "./img/aboutImage.jpg";
-import { IoIosFlower } from "react-icons/io";
+import { IconType } from "react-icons";
 
-const About = () => {
+interface AboutProps {
+  image: StaticImageData | string;
+  icon: IconType;
+}
+
+const About: React.FC<AboutProps> = ({ image, icon: Icon }) => {
   return (
-    <div className=" relative md:p-20  w-full h-[100vh]  flex flex-col justify-center md:flex-row items-center">
-      <h1 className="absolute top-12 right-[25%] font-bold z-10 text-6xl">ABOUT US</h1>
-      <div className=" relative md:w-1/2 w-4/5 flex justify-center items-center ">
-        <IoIosFlower className="absolute md:bottom-16 -left-12 text-7xl" />
-        <Image
-          className="rounded-xl md:min-w-[500px] md:max-w-[600px]"
-          src={aboutImage}
-          alt="aboute image"
-        />
-      </div>
-      <div className=" relative h-min md:w-1/2 flex flex-col gap-5 items-center justify-center md:p-16 p-7 text-xs pt-10">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-          repellendus cumque maxime praesentium corrupti veritatis minus quasi
-          impedit sequi ducimus. Numquam ut perspiciatis ea ab illo asperiores
-          magni, architecto minus!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
-          repellendus cumque maxime praesentium corrupti veritatis minus quasi
-          impedit sequi ducimus. Numquam ut perspiciatis ea ab illo asperiores
-          magni, architecto minus!
-        </p>
-        <div className=" absolute bottom-0 right-16 w-16 border-b-8 "></div>
+    <div className="md:p-20 p-10 w-full md:h-[100vh] flex justify-center items-center">
+      <div className="flex gap-10 flex-col md:flex-row items-center md:items-stretch">
+        <div className="relative md:w-1/2 flex justify-center items-center">
+          <Icon className="absolute -bottom-5 -right-5 md:bottom-16 md:-left-12 text-7xl" />
+          <Image
+            className="rounded-xl"
+            src={image}
+            alt="två tjejer sitter vid en dator och arbetar"
+          />
+        </div>
+        <div className="md:w-1/2 flex flex-col justify-between items-center md:items-start gap-5 md:pr-16 text-center md:text-start text-xs">
+          <h1 className="whitespace-nowrap font-bold text-6xl">ABOUT US</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+            repellendus cumque maxime praesentium corrupti veritatis minus quasi
+            impedit sequi ducimus. Numquam ut perspiciatis ea ab illo asperiores
+            magni, architecto minus!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+            repellendus cumque maxime praesentium corrupti veritatis minus quasi
+            impedit sequi ducimus. Numquam ut perspiciatis ea ab illo asperiores
+            magni, architecto minus!
+          </p>
+          <div className=" self-end w-16 border-b-8"></div>
+        </div>
       </div>
     </div>
   );
--- itsystrar/src/app/components/Main/Main.tsx
+++ itsystrar/src/app/components/Main/Main.tsx
@@ -1,9 +1,10 @@
-
+import aboutImage from "../About/img/aboutImage.jpg";
+import { IoIosFlower } from "react-icons/io";
 import About from "../About/About"
 
 const Main = () => {
   return (
-    <div className=""><About /></div>
+    <div className=""><About image= {aboutImage} icon= {IoIosFlower} /></div>
 
   )
 }
